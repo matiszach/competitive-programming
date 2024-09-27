a4e3784f00f06faaab2f11176ac03a08987538d0
Mateusz Kowalski <80004522+matiszach@users.noreply.github.com>
2024-09-27 11:35:26 +0200

    Update drogi rowerowe.cpp

--- POI/XXV (2017-2018)/drogi rowerowe.cpp
+++ POI/XXV (2017-2018)/drogi rowerowe.cpp
@@ -2,82 +2,52 @@
 //*Author Mateusz Kowalski*
 #include <bits/stdc++.h>
 using namespace std;
-const int SIZE=5e4+5;
-int n,m,a,b;
-vector<int> graph[SIZE],r_graph[SIZE],new_graph[SIZE],r_new_graph[SIZE];
+typedef long long ll;
 #define pb push_back
-bitset<SIZE> visited;
+const int MAXN = 1e5 + 7;
+vector<int> G[MAXN], R[MAXN], SCC[MAXN];
+bool visited[MAXN];
 vector<int> postorder;
 void DFS(int x){
-	visited[x]=1;
-	for(int v:graph[x]){
-		if(!visited[v]) DFS(v);
-	}
+	visited[x] = 1;
+	for(int &v : G[x]) if(!visited[v]) DFS(v);
 	postorder.pb(x);
 }
-int counter=0;
-int colors[SIZE];
-int sizes[SIZE];
-int values[SIZE];
-void DFS2(int x){
-	visited[x]=1;
-	colors[x]=counter;
-	++sizes[counter];
-	for(int v:r_graph[x]){
-		if(!visited[v]) DFS2(v);
-	}
-}
-void DFS4(int x){
-	visited[x]=1;
-	for(int v:new_graph[x]){
-		if(!visited[v]) DFS4(v);
-	}
-	postorder.pb(x);
+int colors[MAXN];
+int color;
+int res[MAXN];
+void rDFS(int x){
+	colors[x] = color;
+	++res[color];
+	for(int &v : R[x]) if(!colors[v]) rDFS(v);
 }
 int main()
 {
 	ios_base::sync_with_stdio(0);
-	cin>>n>>m;
-	for(int i=0;i<m;++i){
-		cin>>a>>b;
-		graph[a].pb(b);
-		r_graph[b].pb(a);
+	int n, m; cin >> n >> m;
+	for(int i = 1; i <= m; ++i){
+		int a, b; cin >> a >> b;
+		G[a].pb(b);
+		R[b].pb(a);
 	}
-	for(int i=1;i<=n;++i){
-		if(!visited[i]){
-			DFS(i);
+	for(int i = 1; i <= n; ++i){
+		if(!visited[i]) DFS(i);
+	}
+	reverse(postorder.begin(), postorder.end());
+	for(int &v : postorder){
+		if(!colors[v]){
+			++color;
+			rDFS(v);
 		}
 	}
-	visited.reset();
-	for(int i=postorder.size()-1;i>=0;--i){
-		if(!visited[postorder[i]]){
-			++counter;
-			DFS2(postorder[i]);
+	for(int i = 1; i <= n; ++i){
+		for(int &v : G[i]){
+			if(colors[i] != colors[v]) SCC[colors[i]].pb(colors[v]);
 		}
 	}
-	for(int i=1;i<=n;++i){
-		for(int v:graph[i]){
-			if(colors[i]!=colors[v]){
-				new_graph[colors[i]].pb(colors[v]);
-				r_new_graph[colors[v]].pb(colors[i]);
-			}
-		}
+	for(int i = color; i >= 1; --i){
+		for(int &v : SCC[i]) res[i] += res[v];
 	}
-	visited.reset();
-	postorder.clear();
-	for(int i=1;i<=counter;++i){
-		if(!visited[i]){
-			DFS4(i);
-		}
-	}
-	visited.reset();
-	for(int e:postorder){
-		for(int v:new_graph[e]){
-			values[e]+=values[v]+1;
-		}
-		values[e]+=sizes[e]-1;
-	}
-	for(int i=1;i<=n;++i){
-		cout<<max(values[colors[i]],sizes[colors[i]]-1)<<"\n";
-	}
+	for(int i = 1; i <= n; ++i) cout << res[colors[i]] - 1 << "\n";
 }
+
